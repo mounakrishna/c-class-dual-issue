e1cd0f0b373b3e1b2e5d3d7a5f0e301defbc3a4e
Mouna krishna G <38425890+mounakrishna@users.noreply.github.com>
2025-01-28 17:16:05 +0530

    Removed a replicated line in configure.py

--- configure/configure.py
+++ configure/configure.py
@@ -219,7 +219,6 @@ def capture_compile_cmd(foo, isa_node, debug_spec, grouping_spec):
     macros += ' desize='+str(foo['depoch_size'])
     macros += ' num_harts='+str(foo['num_harts'])
     macros += ' num_issue='+str(foo['num_issue'])
-    macros += ' instr_queue='+str(foo['instr_queue'])
     macros += ' microtrap_support'
 
     wawid = foo['isb_sizes']['isb_s3s4']+foo['isb_sizes']['isb_s4s5']
